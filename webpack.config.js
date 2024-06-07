7a3550b56578b845ad61ec9cfb60c9f587fdf8dc
doorme777 <137806586+doorme777@users.noreply.github.com>
2024-06-07 15:11:39 -0600

    🐛 [FIX] Se cambiaron el nombre de output

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
+    filename: "dist.js",
     publicPath: "/",
   },
   resolve: {
